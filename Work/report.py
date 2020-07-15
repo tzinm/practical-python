c591988ee3a91285bd0792913b51b50e5c46d702
tzinm <aritz.miranda@gmail.com>
2020-07-16 01:40:26 +0200

    Reached exercise 2.15 - A practical enumerate() example

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,67 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+def read_portfolio(filename):
+    portfolio = []
+    portfolio_dic = {}
+    
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        for row in rows:
+            portfolio_dic[header[0]] = row[0]
+            portfolio_dic[header[1]] = int(row[1])
+            portfolio_dic[header[2]] = float(row[2])
+            portfolio.append(portfolio_dic)
+            portfolio_dic = {}
+    return portfolio
+
+def read_prices(filename):
+    prices = {}
+    try:
+        with open(filename, 'rt') as f:
+            rows = csv.reader(f)
+            for row in rows:
+                prices[row[0]] = float(row[1])
+    except IndexError:
+        pass
+    return prices
+
+def gain_loss(portfolio, prices):
+    gain_loss = []
+    gain_loss_dic = {}
+    gain_loss_in_dic = {}
+    
+    for row in portfolio:
+        differences = round(prices[row['name']] - row['price'],2)
+        gain_loss_in_dic['adquisition'] = float(row['price'])
+        gain_loss_in_dic['now'] = float(prices[row['name']])
+        gain_loss_in_dic['difference'] = differences
+        if differences > 0:
+            gain_loss_in_dic['g/l'] = 'gain'
+        else:
+            gain_loss_in_dic['g/l'] = 'loss'
+        gain_loss_dic[row['name']] = gain_loss_in_dic
+        gain_loss.append(gain_loss_dic)
+        gain_loss_dic = {}
+        gain_loss_in_dic = {}
+    
+    return gain_loss
+
+def make_report(portfolio, prices):
+    result = []
+
+    for row in portfolio:
+        linea = (row['name'], row['shares'], prices[row['name']], round(prices[row['name']] - row['price'],2))
+        result.append(linea)
+    return result
+        
+headers = ('Name', 'Shares', 'Price', 'Change')
+
+print('%10s %10s %10s %10s' % headers)
+print(('-' * 10 + ' ') * len(headers))
+for name, shares, price, change in make_report(read_portfolio('Data/portfolio.csv'), read_prices('Data/prices.csv')):
+    print(f'{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}')
