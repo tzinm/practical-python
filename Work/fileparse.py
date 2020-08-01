c1885d2c8eec6ca91d20b9c51a2300805cc5f454
tzinm <aritz.miranda@gmail.com>
2020-08-01 12:29:09 +0200

    Reached exercise 3.15 - main() functions

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,47 @@
 # fileparse.py
 #
 # Exercise 3.3
+import csv
+
+def parse_csv(filename, select=None, types=None, has_headers=True, separator=',', silence_errors=False):
+    '''
+    Parse a CSV file into a list of records
+    '''
+    with open(filename) as f:
+        if select and has_headers == False:
+            raise RuntimeError("select argument requires column headers")
+
+        rows = csv.reader(f, delimiter=separator)
+        
+        #Read the header of the files
+        headers = next(rows) if has_headers else []
+
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        
+        records = []
+        
+        for rowno, row in enumerate(rows, 1):
+            if not row:     #Skipping rows with no data
+                continue
+
+            if select:
+                row = [row[index] for index in indices]
+
+            if types:
+                try:
+                    row = [func(val) for func,val in zip(types, row)]
+                except ValueError as e:
+                    if not silence_errors:
+                        print(f"Row {rowno}: Couldn't convert {row}")
+                        print(f"Row {rowno}: Reason {e}")
+                    continue
+
+            if headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
+            records.append(record)
+
+    return records
--- Work/pcost.py
+++ Work/pcost.py
@@ -4,19 +4,14 @@
 
 import csv
 import sys
+import report
 
 def portfolio_cost(filename):
+    portfolio = report.read_portfolio(filename)
     total_cost = 0
-    with open(filename, 'rt') as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        for rowno, row in enumerate(rows, start=1):
-            record = dict(zip(headers, row))
-            try:
-                total_cost += int(record['shares']) * float(record['price'])
-            except ValueError:
-                print(f'Row {rowno}: Bad row: {row}')
-        return total_cost
+    for element in portfolio: 
+        total_cost += int(element['shares']) * float(element['price'])
+    return total_cost
 
 if len(sys.argv) == 2:
    filename = sys.argv[1]
